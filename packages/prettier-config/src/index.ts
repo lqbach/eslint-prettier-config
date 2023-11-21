e099751d8349b561a15df711e09cfc6763e6b48a
lqbach <long_quan_bach@hotmail.com>
2023-11-21 00:10:53 -0500

    feat: remove import sorting with Prettier

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,23 +1,6 @@
-import { PrettierConfig } from "@ianvs/prettier-plugin-sort-imports"
+import { Config } from "prettier"
 
-import { PrettierConfigParams } from "./types"
-import { cleanSortImportsArray } from "./utils"
-
-export default function config(
-  params: PrettierConfigParams = {},
-): PrettierConfig {
-  let defaultImportOrder = [
-    "<BUILTIN_MODULES>", // Node.js built-in modules
-    "<THIRD_PARTY_MODULES>", // Imports not matched by other special words or groups.
-    "<TYPES>", // Types
-    "<TYPES>^[.]",
-    "^[.]", // relative imports
-  ]
-
-  const importOrder = cleanSortImportsArray(
-    params.importOrder ?? defaultImportOrder,
-  )
-
+export default function config(): Config {
   return {
     printWidth: 80,
     tabWidth: 2,
@@ -25,8 +8,5 @@ export default function config(
     semi: false,
     singleQuote: false,
     bracketSpacing: true,
-
-    plugins: ["@ianvs/prettier-plugin-sort-imports"],
-    importOrder: importOrder,
   }
 }
